a9cff9198e58605ca4fe382cce0d1f1e6120edf1
tyribeiro <thais.earibeiro@gmail.com>
2022-10-23 22:07:29 -0700

    local storage js
    saved theme to local storage so page can refresh correctly

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ function initThemeSelector(){
     //define constants
     const themeSelect = document.getElementById("theme-select");
     const themeStyleLink = document.getElementById("theme-link");
+    const currentTheme = localStorage.getItem("theme") || "default";
 
     //function to activate theme
     function activateTheme(theme){
@@ -13,10 +14,13 @@ function initThemeSelector(){
     // by passing in the value that user clicked
     themeSelect.addEventListener("change", () =>{
         activateTheme(themeSelect.value);
+        // save the theme onto local storage 
+        localStorage.setItem("theme", themeSelect.value)
     });
 
-
-    activateTheme(themeSelect.value);
+    //when page refreshes, saves the current theme value in drop down selector
+    themeSelect.value = currentTheme;
+    activateTheme(currentTheme);
 }
 
 //call as soon as page loads
